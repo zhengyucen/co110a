982716299e34fa9c1875545ffb7b83ea97210d2b
zhengyucen <lol667980@gmail.com>
2021-11-03 15:10:36 +0800

    code1

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@2    // r2=0(歸0)
+M=0
+@0   //r0=M 
+D=M
+@21  //IF r0=0 , 跳至 r21結束  0*任何數=0
+D;JEQ 
+@1  //r1=M
+D=M
+@3
+M=D //計算次數(最後會歸0)
+@21 //IF r1=0,跳至 r21結束 任何數*0=0
+D;JEQ
+@14 //LOOP 
+D;JNE
+@0
+D=M
+@2
+M=M+D // 相加x次(乘法)
+@3
+M=M-1
+D=M
+@12 //LOOP
+D;JNE
+@21 //卡迴圈
+0;JMP
